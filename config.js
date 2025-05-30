43a216b619ef9f50f4d3ee3f19432d307cfbad06
bitrend <alvaroalverdi+github@gmail.com>
2025-05-29 22:22:56 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-// API Keys (usa variables de entorno en producción)
+// Reemplaza con tus claves si es necesario
 export const COINGECKO_API = 'https://api.coingecko.com/api/v3';
 export const DEXTOOLS_API = 'https://api.dextools.io/v1';
