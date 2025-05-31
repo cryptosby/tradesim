ea397485943aa5e846d530e95fc9dc97b116e090
bitrend <alvaroalverdi+github@gmail.com>
2025-05-30 18:50:15 -0600

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,4 @@
+// Configuración de APIs (¡Reemplaza con tus keys si es necesario!)
+export const COINGECKO_API = 'https://api.coingecko.com/api/v3';
+export const DEXTOOLS_API = 'https://api.dextools.io/v1';
+export const BINANCE_WS = 'wss://stream.binance.com/ws';
